17249941c973ec2f04a75beeade3e3cfe78f7e40
Santiago Marín <esmarina@unal.edu.co>
2020-09-27 10:23:38 -0500

    Creación de la barra de navegación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,11 +18,15 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input'; 
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmpleadoFormComponent
+    EmpleadoFormComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
@@ -35,7 +39,8 @@ import { MatTableModule } from '@angular/material/table';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatTableModule
+    MatTableModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
