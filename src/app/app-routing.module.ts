569a5423591cd9f4068cc071dd47e67f0c8a583d
Santiago Marín <esmarina@unal.edu.co>
2020-09-27 00:12:53 -0500

    Clase 'Empleado' creada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { pathToFileURL } from 'url';
 
 import { EmpleadoFormComponent } from './components/empleado-form/empleado-form.component';
 
---
+++ src/app/models/Empleado.ts
@@ -0,0 +1,11 @@
+export class Empleado {
+  id: string;
+  name: string;
+  contractTypeName: string;
+  roleId: number;
+  roleName: string;
+  roleDescription: string;
+  hourlySalary: number;
+  monthlySalary: number;
+  annualSalary: number;
+}
